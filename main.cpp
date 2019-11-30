d8699e8f3924b090ef523f02680b40141002daea
Lara <49004243+LaraPompei@users.noreply.github.com>
2019-11-29 23:47:15 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -18,9 +18,8 @@ using namespace std;
  *                                                                                            *
  **********************************************************************************************
  */
-void leitura(vector<registro>& lista){
+void leitura(vector<registro>& lista, vector<string>& linha, vector<vector<string>>& linhas){
     fstream file, bgg;
-    int linha = 0;
     if(TAM>17065)
 	cout<<"tamanho especificado maior do que o arquivo games_detailed_info.csv"<<endl;
     file.open("games_detailed_info.csv", ios::in);
@@ -29,12 +28,6 @@ void leitura(vector<registro>& lista){
     if(bgg.is_open()){
         int num = 0;//numero de linhas no arquivo
         string str,palavra;
-        while(bgg)
-        {
-            getline(bgg,str);
-            num++;
-        }
-	cout<<"Total de linhas no bgg-13m-reviews.csv: "<<num<<endl;
         //reinicializando o arquivo
         bgg.clear();
         bgg.seekg(0, ios::beg);
@@ -44,26 +37,26 @@ void leitura(vector<registro>& lista){
         int random = 17065-TAM;
         random = rand() % random;
         int contN = 0;
-        while(getline(bgg, str))
-        {
-            stringstream s(str);
-            if(contN >= random && contN <=random+TAM)
-            {
-                registro reg;
-                getline(s, palavra, ';');
-                getline(s, palavra, ';');
-                reg.setUser(palavra);
-                getline(s, palavra, ';');
-                if(palavra>="0.0" && palavra<="10.0")
-                {
-                    reg.setRating(stoi(palavra));
-                    lista.push_back(reg);
-                }
-                else
-                    contN--;
+	bgg.seekg(random, ios::beg);
+	getline(file,str);
+        while(getline(file, str)){
+	    stringstream s(str);
+            registro reg;
+            getline(s, palavra, ',');
+	    reg.setId(stoi(palavra));
+            getline(s, palavra, ',');
+            reg.setUser(palavra);
+            getline(s, palavra, ',');
+            if(palavra>="0.0" && palavra<="10.0"){
+           	reg.setRating(stoi(palavra));
+           	lista.push_back(reg);
             }
+            else
+               contN--;
             contN++;
             s.clear();
+	    if(contN<TAM)
+	    	break;
         }
     }
     else
@@ -71,77 +64,66 @@ void leitura(vector<registro>& lista){
 
     cout<<"abrindo games_detailed_info.csv"<<endl;
     if(file.is_open()){
-        int num = 0;//numero de linhas no arquivo
+	//linhas eh uma matrix que armazena todas as linhas e colunas
+
+        int num = 17065;//numero de linhas no arquivo
         string str, palavra; //str eh usada para armazenar uma linha e palavra cada uma das colunas de str
         //conta a quantidade de linhas no arquivo(inutil, foi so pra rodar 1 vez)
-	while(file){
-            getline(file,str);
-            num++;
-	}
-	cout<<"total de linhas do games_detailed_info: "<<num<<endl;
+	char separador = ',';
+        //delimitador 2 e 3 antes ou depois da , indicam uma coluna
+        char delimitador1 = ',';
+        char delimitador2 = '"';
 
-        //reinicializando o arquivo
-        file.clear();
-        file.seekg(0, ios::beg);
+	int i = 0;
+	while(!file.eof()){
+	    getline(file, str); //le cada linha
+	    stringstream s(str); //coloca a linha lida em uma stringstream
+	    //lista eh um vetor que vai armazenar a linha completa
+	    bool flag = 0;
 
-        getline(file,str);//pula primeira linha
-        int cont = 0;
-	vector<registro> ::iterator it;
-	for (it = lista.begin(); it != lista.end(); ++it){
-            while(getline(file, str) && cont<=it->getId())
-            {
-            	stringstream s(str);
-            	if(cont == it->getId())
-            	{
-                    registro reg;
-                    for(int i = 1; i<= 31; i++){
-                    	getline(s, palavra, ',');
-                    }
-                    getline(s,palavra,',');
-		    //reg.setDescricao(str);
-	       }
-               cont++;
-               s.clear();
-           }
-	}
+	    while(getline(s, str)){
+	    	int inicio = 0;
+		for(i = 0; i <str.length(); i++){
+		    if(str[i] == separador){
+		    	linha.push_back(str.substr(inicio, i-inicio));
+			inicio = i+1;
+		    }
+		    else  if(str[i] == delimitador1 || str[i] == delimitador2){
+		    	for(i = i++; i<=str.length(); i++){
+			    if(i==str.length()){
+			    	flag = 1;
+				linha.push_back(str.substr(inicio, (i-1)-inicio));
+				break;
+			    }
+			    if((str[i] == delimitador2 || str[i]== delimitador1)&& str[i+1] == separador){
+			    	linha.push_back(str.substr(inicio, i-inicio+1));
+				i++;
+				inicio = i+1;
+			    }
+			}
+			if(i>str.length())
+			    break;
+		    }
+		}
+
+		if(i - inicio != 0 && flag == false){
+		    linha.push_back(str.substr(inicio, (i-1)-inicio));
+		}
+
+	    }
+    	    linhas.push_back(linha);
+	 }	    
     }
     else
 	 cout<<"erro ao abrir o arquivo info"<<endl;
     file.close();
-/*
-    cout<<"abrindo o bgg-13m-reviews.csv"<<endl; 
-    if(bgg.is_open()){
-        linha = 0;
-	string str, palavra;
-	vector<registro>::iterator aux = lista.begin();
-	getline(bgg, str); //pula primeira linha do arquivo
-	while(getline(bgg, str)){
-            stringstream s(str);
-	    getline(s,palavra, ',');
-      	    cout<<"aux->getId() = "<<endl;
-	    cout<<aux->getId()<<endl;
-	    cout<<stoi(palavra)<<endl;
-	    if(stoi(palavra) == aux->getId()){
-		cout<<"aux->getId() = "<<aux->getId()<<endl;
-		while(linha<TAM){
-	    	    getline(s, palavra, ',');
-                    aux->setUser(palavra);
-                    getline(s, palavra, ',');
-		    aux->setRating(stoi(palavra));
-		    linha++;
-		}
-		break;
-	    }
-    	} 
-        cout<<endl<<"linhas: "<<linha<<endl;
-    }
-    else
-	cout<<"Erro ao abrir o bgg-13m-reviews.csv"<<endl;
-	*/
     bgg.close();
 }
 
 int main(){
     vector<registro> lista;
-    leitura(lista);
+    vector<string> linha;
+    vector<vector<string>> linhas;
+    leitura(lista,linha, linhas);
+    
 }
